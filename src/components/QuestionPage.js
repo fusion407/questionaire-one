def7a569f441c93d1e97445dfc236471e8ab5542
fusion407 <fusion407@gmail.com>
2024-04-10 16:49:56 -0500

    initial commit

---
+++ src/components/QuestionPage.js
@@ -0,0 +1,12 @@
+import QuestionPageForm from './QuestionPageForm';
+
+function QuestionPage() {
+    return(
+        <div class="QuestionPage">
+            <h1>Questionaire Page</h1>
+            <QuestionPageForm />
+        </div>
+    )
+}
+
+export default QuestionPage;
---
+++ src/components/QuestionPageForm.js
@@ -0,0 +1,15 @@
+function QuestionPageForm() {
+    return (
+        <div class="QuestionForm">
+            <h2>Question Form Title</h2>
+            <li>question 1</li>
+            <input placeholder="answer 1"></input>
+            <li>question 2</li>
+            <input placeholder="answer 2"></input>
+            <li>question 3</li>
+            <input placeholder="answer 3"></input>
+        </div>
+    )
+}
+
+export default QuestionPageForm;
