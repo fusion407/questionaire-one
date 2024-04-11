cf54c9e72909afa772c553fbdf532a6f06e80c2b
fusion407 <fusion407@gmail.com>
2024-04-11 02:28:12 -0500

    added questions, changed styles, finished initial layout

--- src/components/QuestionPage.js
+++ src/components/QuestionPage.js
@@ -3,7 +3,6 @@ import QuestionPageForm from './QuestionPageForm';
 function QuestionPage() {
     return(
         <div class="QuestionPage">
-            <h1>Questionaire Page</h1>
             <QuestionPageForm />
         </div>
     )
--- src/components/QuestionPageForm.js
+++ src/components/QuestionPageForm.js
@@ -1,13 +1,32 @@
 function QuestionPageForm() {
     return (
         <div class="QuestionForm">
-            <h2>Question Form Title</h2>
-            <li>question 1</li>
-            <input placeholder="answer 1"></input>
-            <li>question 2</li>
-            <input placeholder="answer 2"></input>
-            <li>question 3</li>
-            <input placeholder="answer 3"></input>
+                <h1 className="FormTitle">Questionaire</h1>
+            <div class="ContactDetails">
+                <h2>First name:</h2>
+                <input></input>
+                <h2>Last name:</h2>
+                <input></input>
+                <h2>Email:</h2>
+                <input></input>
+                <h2>Street Address:</h2>
+                <input></input>
+                <h2>City:</h2>
+                <input></input>
+                <h2>Zip Code:</h2>
+                <input></input>
+            </div>
+            <div class="WaterQuestions">
+                <h2># of bathrooms in home:</h2>
+                <input></input>
+                <h2>Do you have city or well water?</h2>
+                <input></input>
+                <h2>Noticed problems:</h2>
+                <input></input>
+                <h2>Upload images of shut off valve:</h2>
+                <input></input>
+            </div>
+                <button class="button" type="submit">Submit</button>
         </div>
     )
 }
